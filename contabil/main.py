261f50a742209ffde29589d12c0b3af33628f25c
gilberto-domingos <domingoshot@hotmail.com>
2024-10-15 15:43:54 -0300

    finished example graphics report (#93665)

--- contabil/main.py
+++ contabil/main.py
@@ -3,9 +3,11 @@ from streamlit_option_menu import option_menu
 
 st.set_page_config(page_title="Mosimann", page_icon=":bar_chart:", layout="wide")
 
+
 def load_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
 
 load_css("css/style.css")
 
@@ -28,7 +30,6 @@ with st.sidebar:
                            menu_icon="cast",
                            default_index=0)
 
-
 if selected == "Home":
     import pagesx.homex as homex
     homex.show()
@@ -47,6 +48,10 @@ elif selected == "Gestão de Pessoas":
 elif selected == "Solicitações":
     st.title("Solicitações")
 elif selected == "Método APLA":
-    st.title("Método APLA")
+    import pagesx.apla as apla
+    apla.show()
 elif selected == "Relatórios":
     st.title("Relatórios")
+    import pagesx.report as report
+    report.show()
+
