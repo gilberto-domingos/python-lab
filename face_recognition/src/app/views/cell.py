4ac6c96b070484b7f9e95d56ecb8b434edfed64a
gilberto-domingos <domingoshot@hotmail.com>
2024-12-18 19:39:37 -0300

    Remove files from index, now ignored

--- face_recognition/src/app/views/cell.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-
-def show():
-    st.subheader("Cadastro de Células")
-    cod_cell = st.text_input("Código : ")
-    name_cell = st.text_input("Nome da célula : ")
-    email_cell = st.text_input("Email da célula : ")
