69503dd706b1eabd88abb15df655871c5aa8c287
gilberto-domingos <domingoshot@hotmail.com>
2024-12-18 19:39:37 -0300

    Remove files from index, now ignored

--- contabil/src/app/views/employee.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-
-def show():
-    st.subheader("Cadastro de funcionários")
-    cod_employee = st.text_input("Código :")
-    name_employee = st.text_input("Nome :")
-    email_employee = st.text_input("Email:")
-    phone_employee = st.text_input("Celular :")
--- contabil/src/database/test_db_connection.py
+++
@@ -1,7 +0,0 @@
-conn = psycopg2.connect(
-    host="172.18.0.3",  # IP do container
-    port="5432",
-    database="contabil",
-    user="mmss",
-    password="mmssmmnn"
-)
