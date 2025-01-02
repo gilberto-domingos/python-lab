7b05bddbd783e0957af2b04f33ed0977340b9e90
gilberto-domingos <domingoshot@hotmail.com>
2025-01-02 17:52:58 -0300

    face recognition (#87866)

--- contabil/src/app/views/face.py
+++ contabil/src/app/views/face.py
@@ -1,12 +1,6 @@
 import streamlit as st
-# from src.app.views.face_create import register_face
-# from src.app.views.face_login import login_face
-# from src.app.views import register_face, login_face
-
-# from .face_create import register_face
-# from .face_login import login_face
 from src.app.views.face_create import register_face
-from src.app.views.face_create import login_face
+from src.app.views.face_login import login_face
 
 
 from src.app.models.operator_model import Operator
