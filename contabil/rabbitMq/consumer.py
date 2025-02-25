844273a6b06e9d9b9af0ec3d0d2e6353c03a483e
gilberto-domingos <domingoshot@hotmail.com>
2025-02-25 11:34:25 -0300

    refatorate docker and implementing rabbitmq (#87882)

---
+++ contabil/rabbitMq/consumer.py
@@ -0,0 +1,54 @@
+import pika
+import json
+import os
+from dotenv import load_dotenv
+
+
+class RabbitMqConsumer:
+    def __init__(self, callback) -> None:
+        self.__host = "localhost"
+        self.__port = 5672
+        self.__username = os.getenv("RABBITMQ_USER")
+        self.__password = os.getenv("RABBITMQ_PASSWORD")
+        self.__queue = "data_queue3"
+        self.__callback = callback
+        self.__channel = self.__create_channel()
+
+    def __create_channel(self):
+        connection_parameters = pika.ConnectionParameters(
+            host=self.__host,
+            port=self.__port,
+            credentials=pika.PlainCredentials(
+                username=self.__username,
+                password=self.__password
+            )
+        )
+
+        channel = pika.BlockingConnection(connection_parameters).channel()
+        channel.queue_declare(
+            queue=self.__queue,
+            durable=True,
+            arguments={
+                "x-overflow": "reject-publish"
+            }
+        )
+
+        channel.basic_consume(
+            queue=self.__queue,
+            auto_ack=True,
+            on_message_callback=self.__callback
+        )
+
+        return channel
+
+    def start(self):
+        print(f'Listen RabbitMQ on Port 5672')
+        self.__channel.start_consuming()
+
+
+def my_callback(ch, method, properties, body):
+    print(body)
+
+
+rabbitmq_consumer = RabbitMqConsumer(my_callback)
+rabbitmq_consumer.start()
---
+++ contabil/rabbitMq/publisher.py
@@ -0,0 +1,44 @@
+from typing import Dict
+import pika
+import json
+import os
+from dotenv import load_dotenv
+
+
+class RabbitMqPublisher:
+    def __init__(self) -> None:
+        self.__host = "localhost"
+        self.__port = 5672
+        self.__username = os.getenv("RABBITMQ_USER")
+        self.__password = os.getenv("RABBITMQ_PASSWORD")
+        self.__exchange = "data_exchange"
+        self.__routing_key = "RK"
+        self.__channel = self.__create_channel()
+
+    def __create_channel(self):
+        connection_parameters = pika.ConnectionParameters(
+            host=self.__host,
+            port=self.__port,
+            credentials=pika.PlainCredentials(
+                username=self.__username,
+                password=self.__password
+            )
+        )
+
+        channel = pika.BlockingConnection(connection_parameters).channel()
+        return channel
+
+    def send_message(self, body: Dict):
+        self.__channel.basic_publish(
+            exchange=self.__exchange,
+            routing_key=self.__routing_key,
+            body=json.dumps(body),
+            properties=pika.BasicProperties(
+                delivery_mode=2
+            )
+        )
+
+
+rabbitMqPublisher = RabbitMqPublisher()
+rabbitMqPublisher.send_message(
+    {"Hello": "World RK22 x-overflow:reject-publish !!!"})
